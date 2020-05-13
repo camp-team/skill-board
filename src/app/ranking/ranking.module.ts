7d523cb1772a303afa26f0836f4715f27cfaf83a
小橋武宣 <tkobashi@tkobashi.local>
2020-05-13 22:03:39 +0900

    [WIP]feat(ranking): UI作成中 #42

--- src/app/ranking/ranking.module.ts
+++ src/app/ranking/ranking.module.ts
@@ -5,9 +5,19 @@ import { RankingRoutingModule } from './ranking-routing.module';
 import { RankingComponent } from './ranking/ranking.component';
 import { ResultComponent } from './result/result.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [RankingComponent, ResultComponent],
-  imports: [CommonModule, RankingRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    RankingRoutingModule,
+    SharedModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class RankingModule {}

55a22b80c49568c15554e2c7f4baaa22621d5222
小橋武宣 <tkobashi@tkobashi.local>
2020-05-23 19:54:43 +0900

    refactor(ResultToSkillPipe): shared.moduleに移動 #50

--- src/app/ranking/ranking.module.ts
+++ src/app/ranking/ranking.module.ts
@@ -8,10 +8,9 @@ import { SharedModule } from '../shared/shared.module';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { ResultToSkillPipe } from '../pipes/result-to-skill.pipe';
 
 @NgModule({
-  declarations: [RankingComponent, ResultComponent, ResultToSkillPipe],
+  declarations: [RankingComponent, ResultComponent],
   imports: [
     CommonModule,
     RankingRoutingModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterFormComponent } from './filter-form/filter-form.component';
 import { BannerComponent } from './banner/banner.component';
+import { ResultToSkillPipe } from '../pipes/result-to-skill.pipe';
 
 @NgModule({
-  declarations: [FilterFormComponent, BannerComponent],
+  declarations: [FilterFormComponent, BannerComponent, ResultToSkillPipe],
   imports: [CommonModule],
-  exports: [FilterFormComponent, BannerComponent],
+  exports: [FilterFormComponent, BannerComponent, ResultToSkillPipe],
 })
 export class SharedModule {}

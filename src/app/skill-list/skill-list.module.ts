dae5ae4f3d7edc00e26123dcad8b17effa2631bc
小橋武宣 <tkobashi@tkobashi.local>
2020-06-06 11:38:38 +0900

    feat(skill-list): style実装 #60

--- src/app/skill-list/skill-list.module.ts
+++ src/app/skill-list/skill-list.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from '@angular/common';
 import { SkillListRoutingModule } from './skill-list-routing.module';
 import { SkillListComponent } from './skill-list/skill-list.component';
 import { SkillListCardComponent } from './skill-list-card/skill-list-card.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [SkillListComponent, SkillListCardComponent],
-  imports: [CommonModule, SkillListRoutingModule],
+  imports: [
+    CommonModule,
+    SkillListRoutingModule,
+    MatGridListModule,
+    MatCardModule,
+  ],
 })
 export class SkillListModule {}

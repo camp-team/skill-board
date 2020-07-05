1c167775c44fa25c54674b0b2c966839648d40fe
小橋武宣 <tkobashi@tkobashi.local>
2020-07-05 17:40:27 +0900

    fix(scraping): 実行ファイル名修正 #88

--- functions/src/scraping/levtech/levtech.scraping-node.ts
+++ functions/src/scraping/levtech/levtech.scraping-node.ts
@@ -1,7 +1,7 @@
 import { LevtechScraping } from './levtech.scraping';
 
 // [Nodeでの単体実行用ファイル]
-// npx ts-node src/scraping/levtech/levtech.scraping.ts
+// npx ts-node src/scraping/levtech/levtech.scraping-node.ts
 
 // tslint:disable-next-line: no-floating-promises
 new LevtechScraping().exec().then((r) => {

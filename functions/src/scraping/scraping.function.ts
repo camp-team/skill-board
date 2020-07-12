86fdb1ff39db7003ca8b2ff03794e698c1123344
小橋武宣 <tkobashi@tkobashi.local>
2020-07-12 21:46:50 +0900

    feat(algolia): 不要functionの削除 #96

--- functions/src/scraping/scraping.function.ts
+++ functions/src/scraping/scraping.function.ts
@@ -2,6 +2,7 @@ import { LevtechScraping } from './levtech/levtech.scraping';
 import { ScrapingFirestore } from './scraping.firestore';
 import * as functions from 'firebase-functions';
 import { ScrapingContext } from './scraping.context';
+import { ScrapingAlgolia } from './scraping.algolia';
 
 const runtimeOpts: functions.RuntimeOptions = {
   timeoutSeconds: 540,
@@ -17,5 +18,7 @@ export const scrapingLevtech = functions
     const context = new ScrapingContext('levtech');
     await new LevtechScraping().exec(context);
     await new ScrapingFirestore().exec(context);
+    await new ScrapingAlgolia().exec(context); // firestoreで採番されたIDを使うので、firestore保存後に実行
+    // TODO 途中でエラーが発生した場合の挙動(例えば、firestore成功後、algoliaでエラー出たらどうする？)
     return res.status(200).json(context.getDataHeader());
   });

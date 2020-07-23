ede60444bd5b2fe472108d31053396df3b496e4b
小橋武宣 <tkobashi@tkobashi.local>
2020-07-23 19:08:01 +0900

    feat(aggregate): trigger系functionsの削除 #33

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,5 @@ admin.initializeApp();
 
 // 追加or更新したらで以下デプロイ
 // firebase deploy --only functions
-export { skillCreate } from './firestore/skill.function';
-export { skillUpdate } from './firestore/skill.function';
-export { skillDelete } from './firestore/skill.function';
 export { scrapingLevtech } from './scraping/scraping.function';
 export { aggregateScrapingData } from './aggregate/aggregate.function';

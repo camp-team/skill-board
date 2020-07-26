8e711765b030b50b85b5097d7faaf9daafb468d3
小橋武宣 <tkobashi@tkobashi.local>
2020-07-26 19:04:33 +0900

    feat(ngx-pie-chart): コメント修正 #110

--- src/app/directives/ngx-pie-chart-zero-margin.directive.ts
+++ src/app/directives/ngx-pie-chart-zero-margin.directive.ts
@@ -5,9 +5,9 @@ import { PieChartComponent } from '@swimlane/ngx-charts';
   selector: '[appNgxPieChartZeroMargin]',
 })
 // ngx-charts-pie-chart専用
-// pie-chartのマージンを削除()
 export class NgxPieChartZeroMarginDirective {
   constructor(@Self() pieChart: PieChartComponent) {
+    // pie-chartのマージンを削除(デフォルトだと余分な空白が入ってしまう)
     pieChart.margins = [0, 0, 0, 0];
   }
 }

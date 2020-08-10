a4eb6ddbb7acce0cba5c7624733e67445f2e309d
小橋武宣 <tkobashi@tkobashi.local>
2020-08-10 17:21:55 +0900

    [WIP]feat(skill-pill): component追加 #72

--- src/app/skill/skill-routing.module.ts
+++ src/app/skill/skill-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SkillComponent } from './skill/skill.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: SkillComponent,
+  },
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
--- src/app/skill/skill/skill.component.ts
+++ src/app/skill/skill/skill.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-skill',
@@ -6,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skill.component.scss'],
 })
 export class SkillComponent implements OnInit {
-  constructor() {}
+  skills: string[];
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((map) => {
+      this.skills = map.get('skills')?.split(',');
+    });
+  }
+
+  onRemoveSkillPill(removeSkillId: string) {
+    console.log('onSkillPillDelete:' + removeSkillId);
+
+    // 該当のskillIdをqueryParamから除外
+    this.updateParams({
+      skills: this.skills
+        .filter((skillId) => skillId !== removeSkillId)
+        .join(','),
+    });
+  }
+
+  private updateParams(params: object) {
+    console.log('updateParams' + JSON.stringify(params));
+    this.router.navigate([], {
+      queryParamsHandling: 'merge',
+      queryParams: params,
+    });
+  }
 }

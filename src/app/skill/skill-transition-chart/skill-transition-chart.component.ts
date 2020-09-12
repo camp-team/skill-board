154afc4c5cd920a34e21239ce327f810109af5a2
小橋武宣 <tkobashi@tkobashi.local>
2020-09-12 17:29:39 +0900

    [WIP]feat(skill): チャートサンプルデータ #115

--- src/app/skill/skill-transition-chart/skill-transition-chart.component.ts
+++ src/app/skill/skill-transition-chart/skill-transition-chart.component.ts
@@ -48,6 +48,28 @@ export class SkillTransitionChartComponent implements OnInit, AfterViewInit {
         ],
         borderWidth: 1,
       },
+
+      {
+        label: '# of Votes',
+        data: [12, 1, 13, 15, 22, 6],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
     ],
   };
 

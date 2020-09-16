553e2425fa95735f82fe1f0bc048e06d8c2bf5c9
小橋武宣 <tkobashi@tkobashi.local>
2020-09-16 20:23:05 +0900

    fix(environment): 本番環境設定の作成 #132

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  // 現状は、開発環境と同一。
+  // サービス公開のタイミングで環境作成 #131
+  firebase: {
+    apiKey: 'AIzaSyCYWU4ccRrMwo0pgL-VrisEAFqLPOAdbLk',
+    authDomain: 'skill-board-bbe02.firebaseapp.com',
+    databaseURL: 'https://skill-board-bbe02.firebaseio.com',
+    projectId: 'skill-board-bbe02',
+    storageBucket: 'skill-board-bbe02.appspot.com',
+    messagingSenderId: '323121777520',
+    appId: '1:323121777520:web:4fa8afb8c60d9badef9db5',
+    measurementId: 'G-10ZZH9657X',
+  },
+  algolia: {
+    appId: 'PU529L6FSF',
+    searchKey: '371420ca707829c272e2a7eccd88a85b',
+  },
 };
